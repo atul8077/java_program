e771eb9736c4edd09e4bbc3f4f68bcaaf6277807
atul maurya <118934556+atul8077@users.noreply.github.com>
2024-08-30 20:56:56 +0530

    m
    revesh a number

--- reversh.java
+++ reversh.java
@@ -1,6 +1,15 @@
 public class reversh {
     public static void main(String args[]){
+        int n=654;
+        int r=0;
+        while(n!=0){
+            int rd=n%10;
         
+            r=r*10+rd;
+            n=n/10;
+        }
+        System.out.println(r);
+
     }
     
 }
