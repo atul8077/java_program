87e06253624dfd64a24d50bd8610beabcd204076
atul maurya <118934556+atul8077@users.noreply.github.com>
2024-08-30 21:55:45 +0530

    prime number

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class prime {
+    public static String prime(int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                return("not prime");
+            }
+        }
+        return("prime");
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(prime(a));
+
+    }
+    
+}
