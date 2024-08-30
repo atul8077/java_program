19ba0bec0a21edb3ec30a0323d82b26dfccb4435
atul maurya <118934556+atul8077@users.noreply.github.com>
2024-08-30 22:02:49 +0530

    no

--- prime.java
+++ prime.java
@@ -10,6 +10,7 @@ public class prime {
     }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number :");
         int a=sc.nextInt();
         System.out.println(prime(a));
 
